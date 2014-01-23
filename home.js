77ccfe944d298b881525e61ffa570f4402f2ed82
Jan Florian Dietrich <florian.dietrich@magicinternet.de>
2014-01-23 16:45:17 +0100

    Added working testsuites for multiple pages

--- home.js
+++ home.js
@@ -1,14 +1,66 @@
-;(function () {
-	var selectors = {
-		logo: '.header--logo'
-	};
+//var baseUrl = 'http://frontend.dev-fe-vagrant.magic-technik.de/';
+//var baseUrl = 'http://www.myvideo.de/';
+var baseUrl = 'http://www.magic-preview.de/';
+//var baseUrl = 'http://frontend.integration.magic-technik.de/';
+var testScripts = {
+  home: 'specs/home.js',
+  serp: 'specs/serp.js'
+};
 
-	casper.test.begin('myvideo.de has a title', 1, function (test) {
-		casper.start('http://www.myvideo.de', function () {
-			test.assertExists(selectors.logo);
-		}).run(function () {
-		test.done();
-	});
-		
-	})
-}());
+var parameter = '?testTrackingLive=true';
+
+var additionalParameters = {
+  serpResults: '&q=Madonna'
+};
+
+var urlParts = {
+  serp: 'search',
+  playerPage: '',
+  tagPage: ''
+};
+
+var casper = require('casper').create( {
+	clientScripts: [
+		'includes/jasmine.js',
+		'includes/jasmine-jquery.js',
+		'includes/jasmine-tap-reporter.js'
+	],
+	verbose: true,
+  logLevel: 'debug'
+});
+
+casper.on('remote.message', function (message) {
+	this.log(message, 'info');
+});
+
+casper.on('page.error', function(msg, trace) {
+    this.log('Error: ' + msg, 'error');
+});
+
+casper.start(baseUrl + parameter, function() {
+    casper.page.injectJs(testScripts.home);
+
+    this.evaluate(function () {
+      $ = jQuery = MV.jQuery;
+      var jasmineEnv = jasmine.getEnv();
+      jasmineEnv.updateInterval = 250;
+      jasmineEnv.addReporter(new jasmine.TapReporter());
+      jasmineEnv.execute();
+	  });
+});
+
+casper.thenOpen(baseUrl + urlParts.serp + parameter + additionalParameters.serpResults, function() {
+    casper.page.injectJs(testScripts.serp);
+
+    this.evaluate(function () {
+      $ = jQuery = MV.jQuery;
+      var jasmineEnv = jasmine.getEnv();
+      jasmineEnv.updateInterval = 250;
+      jasmineEnv.addReporter(new jasmine.TapReporter());
+      jasmineEnv.execute();
+    });
+});
+
+casper.setHttpAuth('myvideo', 'kooyoe1Shain');
+
+casper.run();
